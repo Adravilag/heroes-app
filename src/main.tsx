1e6d3af29fa512477107bd20a87b1e8550473d22
Adrián Dávila <adrian.davilaguerra@gmail.com>
2025-07-09 00:46:26 +0200

    Corregir routing en producción: configurar basename para React Router

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import HeroesApp from "./HeroesApp";
 import { BrowserRouter } from "react-router-dom";
+import { getBasePath } from "./utils/pathUtils";
 import "./i18n";
+
+// Configurar el basename para React Router
+const basename = getBasePath().slice(0, -1); // Remover la barra final
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <HeroesApp />
     </BrowserRouter>
   </StrictMode>
